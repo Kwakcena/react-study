d42710b3148795874a7ad0857c26500a59f8d40a
Kwakcena <rhkrgudwh@gmail.com>
2020-02-20 18:47:25 +0900

    데이터 배열을 컴포넌트 배열로 변환하기
    자바스크립트의 map을 이용하여 컴포넌트로 구성된 배열을 생성할 수 있다.
    
    웹 브라우저에서 잘 나오지만 크롬 개발자 도구를 살펴보면 "key" prop이
    없다는 경고 메세지가 나온다. 이는 무엇일까?

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React, { Component } from "react";
-import ScrollBox from "./ch05/ScrollBox";
+import IterationSample from "./ch06/IterationSample";
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        <ScrollBox ref={ref => (this.scrollBox = ref)} />
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
-        <button onClick={() => this.scrollBox.scrollToTop()}>맨 위로</button>
-      </div>
-    );
+    return <IterationSample />;
   }
 }
 
