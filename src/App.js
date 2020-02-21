61c6761261eb53a2547f66eb08e68945499d699e
Kwakcena <rhkrgudwh@gmail.com>
2020-02-21 19:57:30 +0900

    useReducer
    useReducer는 useState보다 더 다양한 컴포넌트 상황에 따라 다양한 상태를
    다른 값으로 업데이트해 주고 싶을 때 사용하는 Hook 입니다.
    
    리듀서는 현재 상태, 그리고 업데이트를 위해 필요한 정보를 담은 액션값을
    전달받아 새로운 상태를 반환하는 함수입니다. 리듀서 함수에서 새로운
    상태를 만들 때는 반드시 불변성을 지켜 주어야 합니다.
    
    - 파라미터 정보
    useReducer의 첫 번째 파라미터에는 "리듀서 함수"를 넣고, 두 번째
    파라미터에는 "해당 리듀서의 기본값"을 넣어 줍니다.
    이를 통해 state값과 dispatch 함수를 받아 오는데, 여기서 state는 현재
    가리키고 있는 상태, dispatch는 액션을 발생시키는 함수이다.
    
    - 장점
    컴포넌트 업데이트 로직을 컴포넌트 바깥으로 빼낼 수 있다는 것이다.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
-import React, { useState } from "react";
-import Info from "./ch08/Info";
+import React from "react";
+import Counter from "./ch08/Counter";
 
 const App = () => {
-  const [visible, setVisible] = useState(false);
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setVisible(!visible);
-        }}
-      >
-        {visible ? "숨기기" : "보이기"}
-      </button>
-      <hr />
-      {/* visible이 true면 <Info /> 실행, false면 X */}
-      {visible && <Info />}
-    </div>
-  );
+  return <Counter />;
 };
 
 export default App;
