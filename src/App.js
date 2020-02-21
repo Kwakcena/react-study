539e22ed09bbc0e367717657f4c282a14a906a42
Kwakcena <rhkrgudwh@gmail.com>
2020-02-21 19:32:45 +0900

    useEffect 뒷정리하기
    useEffect는 기본적으로 렌더링되고 난 직후마다 실행되며, 두 번째 파라미터
    배열에 무엇을 넣는지에 따라 실행되는 조건이 달라진다.
    
    컴포넌트가 언마운트 되기 전이나 업데이트 되기 직전에 어떠한 작업을
    수행하고 싶다면 useEffect에서 뒷정리(cleanup) 함수를 반환해 주어야 한다.
    
    App.js에서 onClick 이벤트가 발생하게 되면 visible이 true가 되고 <Info />
    컴포넌트가 렌더링 된다. 이때 useEffect 함수가 실행되면서 effect를
    콘솔창에 띄우고 당시의 name값을 출력해준다.
    name을 다른 값으로 바꾸면 컴포넌트가 렌더링될 때마다 뒷정리 함수가
    나타난다. 그리고 업데이트 되기 직전의 값을 출력해준다.
    
    오직 언마운트 될 때만 뒷정리 함수를 호출하고 싶다면 useEffect 함수의 두
    번째 파라미터에 비어있는 배열을 넣어주면 된다.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Info from "./ch08/Info";
 
 const App = () => {
-  return <Info />;
+  const [visible, setVisible] = useState(false);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      >
+        {visible ? "숨기기" : "보이기"}
+      </button>
+      <hr />
+      {/* visible이 true면 <Info /> 실행, false면 X */}
+      {visible && <Info />}
+    </div>
+  );
 };
 
 export default App;
