9fb897c6a8b7aeed2411a8a7de9668edc9aa4a25
Kwakcena <rhkrgudwh@gmail.com>
2020-02-28 16:39:04 +0900

    chapter 09 컴포넌트 스타일링
    가장 흔한 방식, 일반 CSS
    
    특별히 이름을 짓는 규칙을 사용해 클래스 이름을 짓거나 CSS Selector를
    사용해서 스타일을 적용한다. 현재 코드는 CSS Selector를 사용하도록
    App.js와 App.css를 수정했다.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
-import Average from "./ch08/Average";
+import logo from "./logo.svg";
+import "./App.css";
 
-const App = () => {
-  return <Average />;
-};
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <img src={logo} className="logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
