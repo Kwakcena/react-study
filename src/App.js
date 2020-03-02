e0200e4d5d33cd87615e48a6b48a67a779f6402a
Kwakcena <rhkrgudwh@gmail.com>
2020-03-02 18:13:31 +0900

    CSS Module 사용하기
    CSS Module은 CSS를 불러와서 사용할 때 클래스 이름을 고유한 값,
    즉 [파일이름]_[클래스 이름]__[해시값] 형태로 자동으로 만들어서 컴포넌트
    스타일 클래스 이름이 중첩되는 현상을 방지해 주는 기술이다.
    
    -사용법
    따로 설정할 필요없이 .module.css 확장자로 파일을 저장하면 된다.
    
    해당 클래스는 우리가 방금 만든 스타일을 직접 불러온 컴포넌트
    내부에서만 작동한다.
    
    만약 특정 클래스가 웹 페이지에서 전역적으로 사용되는 경우는
    :global 을 앞에 입력하여 글로벌 CSS임을 명시해 줄 수 있다.
    
    CSS Module이 적용된 스타일 파일을 불러오면 객체를 하나 전달받게 되는데
    CSS Module에서 사용한 클래스 이름과 해당 이름을 고유화한 값이 키-값
    형태로 들어 있다.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import SassComponent from "./ch09/SassComponent";
+import CSSModule from "./ch09/CSSModule";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <SassComponent />
+        <CSSModule />
       </div>
     );
   }
