f865df33a30fa52fc73fc6e89c6a27efb9f92926
Kwakcena <rhkrgudwh@gmail.com>
2020-03-02 18:26:32 +0900

    Sass와 함께 사용하기
    .module.scss 확장자를 사용하면 css파일 뿐 아니라 scss 파일에 대해서도
    사용이 가능하다.

--- src/ch09/CSSModule.js
+++ src/ch09/CSSModule.js
@@ -1,6 +1,6 @@
 import React from "react";
 import classNames from "classnames/bind";
-import styles from "./styles/CSSModule.module.css";
+import styles from "./styles/CSSModule.module.scss";
 
 const cx = classNames.bind(styles);
 
